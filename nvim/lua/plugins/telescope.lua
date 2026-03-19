e3384b4a25a1763d8989606bede06406196f2026
Rui Oliveira <rpoliveira02@gmail.com>
2026-03-19 13:42:59 +0000

    fix: add hidden files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,17 +1,39 @@
 local setup = function()
-	local telescope = require("telescope.builtin")
-	vim.keymap.set("n", "<leader>pf", telescope.find_files, { desc = "Telescope find files" })
-	vim.keymap.set("n", "<C-p>", telescope.git_files, { desc = "Telescope find Git files" })
-	vim.keymap.set("n", "<leader>ps", function()
-		telescope.grep_string({ search = vim.fn.input("Grep >") });
-	end)
+    local telescope = require("telescope.builtin")
+
+    local opts = {
+        hidden = true,
+        file_ignore_patterns = {
+            "^%.git/",   -- ignore .git folder
+            "^build/",   -- ignore build folder
+        },
+    }
+
+    vim.keymap.set("n", "<leader>pf", function()
+        telescope.find_files(opts)
+    end, { desc = "Telescope find files (hidden, filtered)" })
+
+    vim.keymap.set("n", "<C-p>", function()
+        telescope.git_files({
+            show_untracked = true,
+        })
+    end, { desc = "Telescope git files" })
+
+    vim.keymap.set("n", "<leader>ps", function()
+        telescope.grep_string({
+            search = vim.fn.input("Grep >"),
+            additional_args = function()
+                return { "--hidden", "--glob", "!.git/*", "--glob", "!build/*" }
+            end,
+        })
+    end)
 end
 
 return {
     "nvim-telescope/telescope.nvim",
     tag = "v0.2.0",
     dependencies = {
-            "nvim-lua/plenary.nvim",
+        "nvim-lua/plenary.nvim",
     },
     config = setup
 }
